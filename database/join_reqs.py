eaa2e28db4f3630add6e76ffab01eeffd93b9372
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2024-08-26 11:50:34 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -1,29 +1,34 @@
 import motor.motor_asyncio
 from info import AUTH_CHANNEL, DATABASE_URI
 
+
 class JoinReqs:
 
     def __init__(self):
-        if DATABASE_URI:
+        global auth_channel
+        if DATABASE_URI and auth_channel:
             self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
             self.db = self.client["JoinReqs"]
-            self.col = self.db[str(AUTH_CHANNEL)]
+            self.col = self.db[str(auth_channel)]
         else:
             self.client = None
             self.db = None
             self.col = None
 
     def isActive(self):
-        if self.client is not None:
-            return True
-        else:
-            return False
+        return self.client is not None
 
     async def add_user(self, user_id, first_name, username, date):
         try:
-            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
-        except:
-            pass
+            await self.col.insert_one({
+                "_id": int(user_id),
+                "user_id": int(user_id),
+                "first_name": first_name,
+                "username": username,
+                "date": date
+            })
+        except Exception as e:
+            logging.error("Error adding user: %s", str(e))
 
     async def get_user(self, user_id):
         return await self.col.find_one({"user_id": int(user_id)})
